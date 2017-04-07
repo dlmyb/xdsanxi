ed6b6c92e4ec6b78876940862c2b2d24a26a3dd9
tuzi_li <bobotu@1122.moe>
2017-04-08 00:38:37 +0800

    add frontend

--- app.py
+++ app.py
@@ -1,71 +1,76 @@
 # -*- coding:utf-8 -*-
 __author__ = 'dlmyb'
 
-from flask import Flask,request,Response,render_template
-import leancloud
 import json
+from flask import Flask, request, Response, jsonify
+import leancloud
 import jwt
 
-app = Flask("__name__")
+app = Flask("__name__", static_folder='static', static_path="")
 
 JWT_KEY = "DLMYB"
 
-# templates 里面扔网页,static 放css,js
-@app.route("/",methods=['GET'])
-def index():
-    return render_template("index.html")
 
-@app.route("/api/login",methods=['POST'])
+@app.errorhandler(404)
+def index(e):
+    return app.send_static_file("index.html")
+
+
+@app.route("/api/login", methods=['POST'])
 def login():
     j = request.json
     username = j.get("username")
     password = j.get("password")
     u = leancloud.User()
     try:
-        u.login(username,password)
+        u.login(username, password)
     except leancloud.LeanCloudError as e:
-        return Response(e.message,401)
-    req = Response({
-        "data":{
-            "username":username,
-            "name":u.get("name"),
-            "jwt":jwt.encode({"username":username,"token":u.get_session_token()},JWT_KEY)
-        }
-    })
-    req.headers["Content-Type"] = "application/json"
-    return req
+        return Response(e.message, 401)
 
-@app.route("/api/grades",methods=['POST'])
+    resp = {
+        "data": {
+            "username": username,
+            "name": u.get("name"),
+            "jwt": jwt.encode({"username": username, "token": u.get_session_token()}, JWT_KEY)
+        }
+    }
+    return jsonify(resp)
+
+
+@app.route("/api/grades", methods=['POST'])
 def grade():
     j = request.json
-    jwtoken = j['jwt']
     try:
-        info = jwt.decode(jwtoken,JWT_KEY)
-    except jwt.InvalidTokenError:
-        return Response("Unexited User",401)
+        jwtoken = j["jwt"]
+        info = jwt.decode(jwtoken, JWT_KEY)
+    except (jwt.InvalidTokenError, KeyError):
+        return Response("Unexited User", 401)
     token = info['token']
     u = leancloud.User.become(token)
     result = {
-        "data":json.loads(u.get("data")),
-        "version":u.get("version")
+        "data": json.loads(u.get("data")),
+        "version": u.get("version")
     }
-    return Response(json.dumps(result),200)
+    return Response(json.dumps(result), 200)
 
-@app.route("/api/check",methods=['POST'])
+
+@app.route("/api/check", methods=['POST'])
 def check():
     j = request.json
-    jwtoken = j['jwt']
-    version = j['version']
     try:
-        info = jwt.decode(jwtoken,JWT_KEY)
+        jwtoken = j['jwt']
+        version = j['version']
+    except KeyError:
+        return Response("miss parms", 400)
+
+    try:
+        info = jwt.decode(jwtoken, JWT_KEY)
     except jwt.InvalidTokenError:
-        return Response("Unexited User",401)
+        return Response("Unexited User", 401)
+
     token = info['token']
     u = leancloud.User.become(token)
     if u.get("version") == version:
-        return Response("",200)
+        return Response("", 200)
     else:
-        return Response("",301)
-
-
-
+        return Response("", 410)
