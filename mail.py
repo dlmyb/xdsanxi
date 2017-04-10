cc036c17ff359b73dc672520e28d9dca2147504e
dlmyb <a@dlmyb.com>
2017-04-10 18:34:45 +0800

    fix bugs

--- mail.py
+++ mail.py
@@ -10,8 +10,10 @@ MAILPASSWORD = os.environ['MAILPASSWORD']
 
 def send(html):
     e = Envelope(
-        from_addr=(u"noreply@xdmsc.club",u"Bug Report"),
+        from_addr=(unicode(MAILACCOUNT),u"Bug Report"),
         subject=u"Bug Report",
         html_body=html
     )
+    e.add_to_addr(u"me@yinbin.ma")
+    e.add_to_addr(u"me@zejun.li")
     e.send(host=MAILHOST,login=MAILACCOUNT,password=MAILPASSWORD,tls=True)
